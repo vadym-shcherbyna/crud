c1b171209f6383a559733d8597ccba26bc933e9d
Vadym Shcherbyna <vadym.shcherbyna@gmail.com>
2022-04-19 15:55:58 +0300

    Update Login Page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -37,7 +37,11 @@
 
             <button class="w-100 btn btn-lg btn-secondary" type="submit">@lang($route . '.buttons.submit')</button>
 
-            <p class="mt-3 mb-3 text-muted">v.1012 &copy; @lang($route . '.copyright')</p>
+            <p class="mt-3 mb-3 text-muted">
+                v.202204
+                <i class="bi bi-git"></i>
+                <a href="https://gitlab.com/Vadym.Shcherbyna/crud-laravel-vue" target="_blank">GitLab Repo</a>
+            </p>
 
         </form>
 
